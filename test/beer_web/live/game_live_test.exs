bbafdde45f6d63882ce570e078b4ffe66c4188bb
Johan Hallstensson <johan.behe.lind@gmail.com>
2019-04-18 08:06:01 +0200

    Test GameLive

---
+++ test/beer_web/live/game_live_test.exs
@@ -0,0 +1,72 @@
+defmodule BeerWeb.GameLiveTest do
+  use ExUnit.Case
+  import Phoenix.LiveViewTest
+
+  setup do
+    Beer.Games.create("existing")
+    Beer.Games.join("existing", "retailer")
+
+    on_exit(fn ->
+      Agent.update(Beer.GameRepo, fn _ -> [] end)
+    end)
+  end
+
+  test "Disconnected render" do
+    {:ok, _view, html} =
+      mount_disconnected(BeerWeb.Endpoint, BeerWeb.GameLive,
+        session: %{name: "existing", role: "retailer"}
+      )
+
+    assert html =~ "Waiting for players…"
+  end
+
+  describe "gameplay" do
+    setup do
+      name = "existing"
+      role = "manufacturer"
+      Beer.Games.join(name, role)
+
+      {:ok, view, _html} =
+        mount(BeerWeb.Endpoint, BeerWeb.GameLive, session: %{name: name, role: role})
+
+      %{view: view}
+    end
+
+    test "update from event", %{view: view} do
+      game = %Beer.Game{
+        name: "updated game",
+        players: %{"manufacturer" => %Beer.Player{role: "updated player"}}
+      }
+
+      send(view.pid, {:game, game})
+
+      html = render(view)
+      assert html =~ "<h1>updated game</h1>"
+      assert html =~ "<h2>updated player</h2>"
+    end
+
+    test "start game", %{view: view} do
+      assert render(view) =~ "Receive delivery"
+    end
+
+    test "receive delivery", %{view: view} do
+      render_click(view, "receive_delivery")
+      assert render(view) =~ "Receive incoming order"
+    end
+
+    test "receive incoming order", %{view: view} do
+      render_click(view, "receive_incoming_order")
+      assert render(view) =~ "Fulfill order"
+    end
+
+    test "fulfill order", %{view: view} do
+      render_click(view, "fulfill_order")
+      assert render(view) =~ "Place order"
+    end
+
+    test "place order", %{view: view} do
+      render_submit(view, "order", %{"units" => "4"})
+      assert render(view) =~ "Receive delivery"
+    end
+  end
+end
